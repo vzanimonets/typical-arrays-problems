4e0e26485ff6c859e8760fc237e32cc872f744a8
Viktar.Zanimonets <vzanimonets@gmail.com>
2020-02-25 20:36:51 +0300

    feat: Typical Arrays Problems task resolution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+exports.min = function min(array) {
+    if (!array || array.length < 1) return 0;
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length < 1) return 0;
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length < 1) return 0;
+    return array.reduce((a, b) => a + b, 0) / array.length;
+};
